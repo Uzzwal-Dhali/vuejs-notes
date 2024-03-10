44713d663fad8747797ff262b30fc3713cbb5e9d
Uzzwal-Dhali <d.uzzwal09@gmail.com>
2024-03-11 01:00:50 +0600

    First Upload

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="header">
+        <h1 class="title">All Notes</h1>
+        <div class="new">+</div>
+      </div>
+      <div class="notes">
+        <div class="note">
+          <h1 class="title">Title</h1>
+          <h4>12th March, 2024</h4>
+          <p class="body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, exercitationem commodi! Itaque ratione iste cum!</p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+  main {
+    .container {
+      max-width: 1300px;
+      margin: 40px auto;
+      background: white;
+      padding: 30px;
+      border-radius: 15px;
+      .header {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 30px;
+      }
+      .notes {
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        .note {
+
+        }
+      }
+    }
+  }
+</style>
